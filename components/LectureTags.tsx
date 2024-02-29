7336c260cb52d63662c2d1ff4f50a81fc53f72fb
Le Quang Thang <lethang7794@gmail.com>
2024-02-29 20:17:30 +0700

    refactor: tags

--- components/LectureTags.tsx
+++ components/LectureTags.tsx
@@ -12,69 +12,84 @@ export function LectureTags({ tags }: { tags: string[] }) {
 }
 
 export function LectureTag({ tag }: { tag: string }) {
-  const sanitizedTag = sanitizeTag(tag);
+  const t = sanitizeTag(tag);
   return (
     <Badge
-      variant={isDemoTag(sanitizedTag) ? "default" : "outline"}
+      variant={isDemoTag(t) ? "default" : "outline"}
       className={cn("h-min capitalize w-fit break-keep", {
         "text-white":
-          isWarningTag(sanitizedTag) ||
-          isAllTag(sanitizedTag) ||
-          isAssociateTag(sanitizedTag) ||
-          isProTag(sanitizedTag) ||
-          isSpecialtyTag(sanitizedTag),
-        "bg-[#46505D]": isAllTag(sanitizedTag),
-        "bg-[#058296]": isProTag(sanitizedTag),
-        "bg-[#3638EE]": isAssociateTag(sanitizedTag),
-        "bg-[#5131B1]": isSpecialtyTag(sanitizedTag),
-        "bg-[#FF9900]": isDemoTag(sanitizedTag),
-        "bg-red-700": isWarningTag(sanitizedTag),
+          isWarningTag(t) ||
+          isAllTag(t) ||
+          isAssociateTag(t) ||
+          isProTag(t) ||
+          isSpecialtyTag(t),
+        "bg-[#46505D]": isAllTag(t),
+        "bg-[#058296]": isProTag(t),
+        "bg-[#3638EE]": isAssociateTag(t),
+        "bg-[#5131B1]": isSpecialtyTag(t),
+        "bg-[#FF9900]": isDemoTag(t),
+        "bg-red-700": isWarningTag(t),
       })}
-      rounded={isDemoTag(sanitizedTag) ? "full" : "sm"}
+      rounded={isDemoTag(t) ? "full" : "sm"}
     >
-      {sanitizedTag}
+      {t}
     </Badge>
   );
 }
 
+const warningMatcher = /NEEDS|DON'T|DONT/gi;
+const allMatcher = /ALL/gi;
+const associateLevelMatcher = /ASSOCIATE|DVA|SAA|SOA/gi;
+const proLevelMatcher = /PRO(?!D)|SAP|DOP/gi;
+const specialtyLevelMatcher = /ANS|SCS/gi;
+const demoMatcher = /DEMO/gi;
+const tagBracesMatcher = /[\[\]]/gi;
+
 function isWarningTag(tag: string): boolean {
-  let match = tag.match(/NEEDS|DON'T|DONT/gi);
+  let match = tag.match(warningMatcher);
   return Boolean(match);
 }
 
 function isAllTag(tag: string): boolean {
-  let match = tag.match(/ALL/gi);
+  let match = tag.match(allMatcher);
   return Boolean(match);
 }
 
 function isAssociateTag(tag: string): boolean {
-  let match = tag.match(/ASSOCIATE|DVA|SAA|SOA/gi);
+  let match = tag.match(associateLevelMatcher);
   return Boolean(match);
 }
 
 function isProTag(tag: string): boolean {
-  let match = tag.match(/PRO(?!D)|SAP|DOP/gi);
+  let match = tag.match(proLevelMatcher);
   return Boolean(match);
 }
 
 function isSpecialtyTag(tag: string): boolean {
-  let match = tag.match(/ANS|SCS/gi);
+  let match = tag.match(specialtyLevelMatcher);
   return Boolean(match);
 }
 
 function isDemoTag(tag: string) {
-  let match = tag.match(/DEMO/gi);
+  let match = tag.match(demoMatcher);
   return Boolean(match);
 }
 
 function sanitizeTag(tag: string): string {
-  return tag
-    .replaceAll(/[\[\]]/gi, "")
-    .toUpperCase()
-    .replaceAll(/UPDATED*/gi, "")
-    .replaceAll("NEEDS", "NEEDS UPDATE")
-    .replaceAll("/", " & ")
-    .replaceAll("AND", "&")
-    .replaceAll("ADVANCEDDEMO", "ADVANCED DEMO")
-    .replaceAll("SHARED", "");
+  return (
+    tag
+      // remove braces
+      .replaceAll(tagBracesMatcher, "")
+      .toUpperCase()
+      // remove update
+      .replaceAll(/UPDATED*/gi, "")
+      .replaceAll("NEEDS", "NEEDS UPDATE")
+      // &
+      .replaceAll("/", " & ")
+      .replaceAll("AND", "&")
+      // add space
+      .replaceAll("ADVANCEDDEMO", "ADVANCED DEMO")
+      // remove redundancy shared
+      .replaceAll("SHARED", "")
+  );
 }
