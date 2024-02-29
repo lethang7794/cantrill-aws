bdb885f89a57282a37026362f9ee6288ded7fee6
Le Quang Thang <lethang7794@gmail.com>
2024-02-29 10:04:03 +0700

    style: tag, time

--- app/page.tsx
+++ app/page.tsx
@@ -59,11 +59,13 @@ export default async function Home() {
 function getSectionTimeHeader(s) {
   return (
     <>
-      <div className="font-mono w-20 text-right">
+      {/* <div className="font-mono w-20 text-right">
         {s.duration.theory.hhmmss}
       </div>
-      <div className="font-mono w-20 text-right">{s.duration.demo.hhmmss}</div>
-      <div className="font-mono w-20 text-right">{s.duration.total.hhmmss}</div>
+      <div className="font-mono w-20 text-right">{s.duration.demo.hhmmss}</div> */}
+      <div className="font-mono min-w-20 text-right">
+        {s.duration.total.hhmmss}
+      </div>
     </>
   );
 }
@@ -71,13 +73,15 @@ function getSectionTimeHeader(s) {
 function getLectureTime(l: any) {
   return (
     <>
-      <div className="font-mono w-20 text-right">
+      {/* <div className="font-mono w-20 text-right">
         {l.isTheory && l.duration ? convertTime(l.duration) : null}
       </div>
       <div className="font-mono w-20 text-right">
         {!l.isTheory && l.duration ? convertTime(l.duration) : null}
+      </div> */}
+      <div className="font-mono min-w-20 text-right">
+        {l.duration ? convertTime(l.duration) : null}
       </div>
-      <div className="font-mono w-20 text-right"></div>
     </>
   );
 }
@@ -103,22 +107,34 @@ const squareBracesRegex = /\[|\]/gi;
 function getShareWith(l: any, cur: string) {
   return (
     <>
-      <div className={cn("w-8", cur == "saa-c03" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "saa-c03" && "font-semibold")}
+      >
         {l.sharedWith["saa-c03"] || cur == "saa-c03" ? "SAA" : null}
       </div>
-      <div className={cn("w-8", cur == "dva-c02" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "dva-c02" && "font-semibold")}
+      >
         {l.sharedWith["dva-c02"] || cur == "dva-c02" ? "DVA" : null}
       </div>
-      <div className={cn("w-8", cur == "soa-c02" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "soa-c02" && "font-semibold")}
+      >
         {l.sharedWith["soa-c02"] || cur == "soa-c02" ? "SOA" : null}
       </div>
-      <div className={cn("w-8", cur == "sap-c02" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "sap-c02" && "font-semibold")}
+      >
         {l.sharedWith["sap-c02"] || cur == "sap-c02" ? "SAP" : null}
       </div>
-      <div className={cn("w-8", cur == "ans-c01" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "ans-c01" && "font-semibold")}
+      >
         {l.sharedWith["ans-c01"] || cur == "ans-c01" ? "ANS" : null}
       </div>
-      <div className={cn("w-8", cur == "scs-c01" && "font-semibold")}>
+      <div
+        className={cn("text-xs min-w-8", cur == "scs-c01" && "font-semibold")}
+      >
         {l.sharedWith["scs-c01"] || cur == "scs-c01" ? "SCS" : null}
       </div>
     </>
