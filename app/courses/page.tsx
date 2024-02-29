14e2e42d4b1d670ceffad2a75a1bf040520a3f9d
Le Quang Thang <lethang7794@gmail.com>
2024-02-29 13:53:45 +0700

    Reformat courses page

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -9,19 +9,25 @@ export default async function CoursesPage() {
     <>
       {Object.entries(courses).map(([level, levelCourse]) => {
         return (
-          <React.Fragment key={level}>
-            <div>Level: {titleCase(level)}</div>
-            {(levelCourse as any[]).map((course) => {
-              const code = course.code.toUpperCase();
-              return (
-                <a key={code} href={`/courses/${code}`}>
-                  <div>
-                    {course.title} ({code.toUpperCase()})
-                  </div>
-                </a>
-              );
-            })}
-          </React.Fragment>
+          <ul key={level}>
+            <li>
+              <div className="text-xl">Level: {titleCase(level)}</div>
+              <ul className="ml-4">
+                {(levelCourse as any[]).map((course) => {
+                  const code = course.code.toUpperCase();
+                  return (
+                    <li key={code}>
+                      <a href={`/courses/${code}`}>
+                        <div>
+                          {course.title} ({code.toUpperCase()})
+                        </div>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
         );
       })}
     </>
