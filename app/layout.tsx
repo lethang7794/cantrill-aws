024f73811f52f80c3a961c1b39dabba21b6a0a8d
Le Quang Thang <lethang7794@gmail.com>
2024-10-25 02:22:14 +0700

    add Hydration Overlay dev tool

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,28 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { HydrationOverlay } from "@builder.io/react-hydration-overlay";
 import { AppProvider } from "@/context/app.context";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Cantrill AWS Courses Curriculum Comparer",
-  // description: "",
+	title: "Cantrill AWS Courses Curriculum Comparer",
+	// description: "",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <AppProvider>{children}</AppProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<HydrationOverlay>
+					<AppProvider>{children}</AppProvider>
+				</HydrationOverlay>
+			</body>
+		</html>
+	);
 }
